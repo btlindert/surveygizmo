e56cd1e8929fdaac54cb8e16974f942fd805f740
btlindert <b.te.lindert@users.noreply.github.com>
2019-11-27 14:59:21 +0100

    Improved merging of files

--- munge/04_merge_data.R
+++ munge/04_merge_data.R
@@ -3,27 +3,33 @@ if (config$merge) {
 
   # Specify columns based on whether is was unhashed.
   if (config$unhash) {
-    merge_cols <- c('study', 'group', 'questionnaire', 'user')
+    merge_cols <- c('study', 'group', 'questionnaire', 'StudyId')
   } else {
-    merge_cols <- c("studyID", "groupID", "questionnaireID", "userID")
+    merge_cols <- c("studyID", "groupID", "questionnaireID", "StudyId")
   }
 
   # Loop through questionnaires, convert to long and merge.
   iter = 1
-  for (f in c(config$to_check, config$not_to_check)) {
-
+  for (questionnaire in c(config$to_check, config$not_to_check)) {
+    
     # Convert file to long format
-    df <- get(f) %>%
-      select(c(starts_with(f), merge_cols)) %>%
-      gather(key = "item", value = "value", -merge_cols)
+    df <- get(questionnaire) %>%
+      select(-starts_with('calc')) %>%
+      gather(key = "item", value = "value", starts_with(questionnaire))
+    
+    # Sometimes endTime is pulled as end_time...
+    names(df) <- str_replace(names(df), 'end_time', 'endTime')
     
     if (iter == 1) {
       merged_data <- df
     } else {
-      merged_data <- rbind(df, merged_data)
+      merged_data <- full_join(df, merged_data)
     }
+    iter = iter + 1
   }
   
   # Write data to csv file
-  write.csv(merged_data, file = config$merged_file)
+  if (!is.na(config$merged_file)) {
+    write.csv(file.path(getwd(), 'data'), file = config$merged_file)
+  }
 }
